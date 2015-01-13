b5e1a695d63fca07540a82a065d09b67645b06d9
Cai Wingfield <cw417@cam.ac.uk>
2015-01-13 16:33:54 +0000

    Is this fixing a bug?

--- Engine/SuLi/searchlightMapping_MEG_source.m
+++ Engine/SuLi/searchlightMapping_MEG_source.m
@@ -7,7 +7,7 @@ function [smm_rs, smm_ps, searchlightRDMs] = searchlightMapping_MEG_source(singl
 %% Get parameters
 
 % Prepare Models
-nModels = size(Models,2);
+nModels = size(Models, 2);
 
 if nModels>1
     modelRDMs_utv = squeeze(unwrapRDMs(vectorizeRDMs(Models)))'; % note by IZ: if Models has 1 model, this is a column vector, else models are put into rows
@@ -17,16 +17,16 @@ end
 
 
 if userOptions.partial_correlation
-    control_for_modelRDMs = modelRDMs_utv(userOptions.partial_modelNumber{1},:);
-    for m = 2:size(userOptions.partial_modelNumber,2)
-        control_for_modelRDMs = [control_for_modelRDMs;modelRDMs_utv(userOptions.partial_modelNumber{m},:)];
+    control_for_modelRDMs = modelRDMs_utv(userOptions.partial_modelNumber{1}, :);
+    for m = 2:size(userOptions.partial_modelNumber, 2)
+        control_for_modelRDMs = [control_for_modelRDMs;modelRDMs_utv(userOptions.partial_modelNumber{m}, :)];
     end
 end
 
 % Picking the right model
 modelNumber = userOptions.modelNumber;
 if nModels > 1 % added by IZ 11-12 to ensure model has more than 1 models to pick from
-    modelRDMs_utv = modelRDMs_utv(modelNumber,:); % added by Li Su 11-2012 in order to test a single model at a time. not best in performance but keep thing simple.
+    modelRDMs_utv = modelRDMs_utv(modelNumber, :); % added by Li Su 11-2012 in order to test a single model at a time. not best in performance but keep thing simple.
 end
 
 nVertices = userOptions.nVertices;
@@ -38,8 +38,8 @@ epochLength = size(singleMesh, 2); % (vertex, time, condition, session)
 
 % Number of DATA points to loop with given the width and time step of
 % searchlight updated by IZ 09-12
-nTimePoints = floor((epochLength - (userOptions.temporalSearchlightWidth*userOptions.toDataPoints)) /...
-    (userOptions.temporalSearchlightResolution*userOptions.toDataPoints*userOptions.temporalDownsampleRate));
+nTimePoints = floor((epochLength - (userOptions.temporalSearchlightWidth * userOptions.toDataPoints)) / ...
+    (userOptions.temporalSearchlightResolution * userOptions.toDataPoints * userOptions.temporalDownsampleRate));
 
 
 %% similarity-graph-map the volume with the searchlight
@@ -65,7 +65,7 @@ smm_rs = zeros([nVertices, nTimePoints, nModels]);
 %     searchlightRDMs = single(zeros(nConditions,nConditions, nVertices, nTimePoints));
 % end
 
-for k=1:length(vertices)
+for k = 1:length(vertices)
     vertex = vertices(k);
     % Determine which vertexes are within the radius of the currently-picked vertex
     
@@ -84,9 +84,9 @@ for k=1:length(vertices)
         % Work out the current time window
         % converted to data points - updated by IZ 09-12
         currentTimeStart = (t - 1) * ...
-            (userOptions.temporalSearchlightResolution*userOptions.temporalDownsampleRate*userOptions.toDataPoints) + 1;
+            (userOptions.temporalSearchlightResolution * userOptions.temporalDownsampleRate * userOptions.toDataPoints) + 1;
         currentTimeWindow = ceil((currentTimeStart : currentTimeStart + ...
-            (userOptions.temporalSearchlightWidth*userOptions.toDataPoints) - 1));
+            (userOptions.temporalSearchlightWidth * userOptions.toDataPoints) - 1));
         
         currentData = singleMesh(verticesCurrentlyWithinRadius, currentTimeWindow, :, :); % (vertices, time, condition, session)
         
@@ -119,14 +119,14 @@ for k=1:length(vertices)
             
         else % data regularization based on algorithm by Diedrichson et al 2011 - updated 12-12 IZ
             tempMesh = reshape(currentData, [], size(currentData, 3), size(currentData, 4));
-            currentData = zeros(size(tempMesh, 1), size(tempMesh, 2)*size(tempMesh,3)); % (data, conditions, sessions)
+            currentData = zeros(size(tempMesh, 1), size(tempMesh, 2) * size(tempMesh, 3)); % (data, conditions, sessions)
             
             % combining session-wise trials
-            k=1;
-            for j=1:size(tempMesh,2)
-                for i=1:userOptions.nSessions
-                    currentData(:,k) = (tempMesh(:,j,i));
-                    k=k+1;
+            kk = 1;
+            for j = 1:size(tempMesh,2)
+                for i = 1:userOptions.nSessions
+                    currentData(:, kk) = (tempMesh(:, j, i));
+                    kk = kk + 1;
                 end
             end
             
@@ -144,7 +144,7 @@ for k=1:length(vertices)
         
         % Locally store the full brain's worth of indexed RDMs. (just
         % lower triangle) added by IZ 09-12
-        if strcmp(userOptions.groupStats,'FFX')
+        if strcmp(userOptions.groupStats, 'FFX')
 %             searchlightRDMs(:,:,vertex, t) = single(tril(squareform(searchlightRDM)));
               searchlightRDMs.(['v_' num2str(vertex)]).(['t_' num2str(t)]).RDM = searchlightRDM;
         else
@@ -152,7 +152,7 @@ for k=1:length(vertices)
         end
         
         if userOptions.partial_correlation
-            [rs, ps] = partialcorr(searchlightRDM', modelRDMs_utv',control_for_modelRDMs','type',userOptions.distanceMeasure,'rows','pairwise');
+            [rs, ps] = partialcorr(searchlightRDM', modelRDMs_utv', control_for_modelRDMs', 'type', userOptions.distanceMeasure, 'rows','pairwise');
         else
             try
                 [rs, ps] = corr(searchlightRDM', modelRDMs_utv', 'type', userOptions.distanceMeasure, 'rows', 'pairwise');
@@ -168,7 +168,7 @@ for k=1:length(vertices)
     end%for:t
     
     % Indicate progress every once in a while...
-    if mod(vertex, floor(length(vertices)/20)) == 0, fprintf('.'); end%if
+    if mod(vertex, floor(length(vertices) / 20)) == 0, fprintf('.'); end%if
     
 end%for:vertices
 
